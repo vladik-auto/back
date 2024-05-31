db2aa86b3a21970aae32cc15f3e822f52e401f30
ExizMan <yurasankin@yandex.ru>
2024-05-31 21:37:13 +0300

    cors fixed

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from src.video.routers import router as router_video
 from fastapi.testclient import TestClient
 # from src.kafka.routers import router as router_kafka
 # from fastapi.middleware.cors import CORSMiddleware
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
+
 from fastapi import WebSocket
 
 app = FastAPIOffline()
@@ -13,18 +14,23 @@ app = FastAPIOffline()
 app.include_router(router_video)
 # app.include_router(router_kafka)
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:8000",
-]
+@app.middleware("http")
+async def add_cors_headers(request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "https://www.teste.com.br, http://localhost:3000"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    return response
 
+origins = [
+    "https://teste.com.br",
+    "http://localhost:3000",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[origins],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
