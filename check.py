5f03c4b1411870c0af77841b461757ae2bec73ee
ExizMan <yurasankin@yandex.ru>
2024-05-31 21:45:35 +0300

    upd origin

--- check.py
+++ check.py
@@ -4,6 +4,7 @@ response = requests.get('http://localhost:8000/')
 
 if 'Access-Control-Allow-Origin' in response.headers:
     print(f"CORS is configured. Allowed origins: {response.headers['Access-Control-Allow-Origin']}")
+    print(response.headers)
 else:
     print("CORS is not configured.")
     print(response.headers)
--- main.py
+++ main.py
@@ -17,15 +17,15 @@ app.include_router(router_video)
 @app.middleware("http")
 async def add_cors_headers(request, call_next):
     response = await call_next(request)
-    response.headers["Access-Control-Allow-Origin"] = "https://www.teste.com.br, http://localhost:3000"
+    response.headers["Access-Control-Allow-Origin"] = "http://localhost:5173"
     response.headers["Access-Control-Allow-Credentials"] = "true"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
     return response
 
 origins = [
-    "https://teste.com.br",
-    "http://localhost:3000",
+
+    "http://localhost:5173",
 ]
 
 app.add_middleware(
